f1de06b1dc5cdb82aab6aece92aa1e26f9827120
webmaster978 <jtjoel11@gmail.com>
2021-05-06 00:09:08 +0200

    modification du head

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ if (isset($_GET["action"])) {
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="#">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-calendar fa-2x text-gray-300"></i>
                 </div>
