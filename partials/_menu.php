38f3ec146917ac3c27dd4e564dcc699623dfcae8
webmaster978 <jtjoel11@gmail.com>
2021-05-18 07:05:54 +0200

    modif

--- partials/_menu.php
+++ partials/_menu.php
@@ -1,12 +1,31 @@
-<div class="sidebar-wrapper">
-    <ul class="nav">
-        <li class="nav-item active  ">
-            <a class="nav-link" href="#">ACCEUIL</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="index.php">Nos produits</a>
-        </li>
-
-
-    </ul>
-</div>
+   <header class="">
+       <nav class="navbar navbar-expand-lg">
+           <div class="container">
+               <a class="navbar-brand" href="index.html">
+                   <h2>Sixteen <em>Clothing</em></h2>
+               </a>
+               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+                   aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                   <span class="navbar-toggler-icon"></span>
+               </button>
+               <div class="collapse navbar-collapse" id="navbarResponsive">
+                   <ul class="navbar-nav ml-auto">
+                       <li class="nav-item active">
+                           <a class="nav-link" href="#">Acceuil
+                               <span class="sr-only">(current)</span>
+                           </a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="#">Nos Produits</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="#">A propos</a>
+                       </li>
+                       <li class="nav-item">
+                           <a class="nav-link" href="#">Contact </a>
+                       </li>
+                   </ul>
+               </div>
+           </div>
+       </nav>
+   </header>
